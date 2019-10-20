d4b7d38756cbdadd741145893a6e4762ae5aaf3c
7nafoaix <shifan.zhou@outlook.com>
2019-10-20 17:11:10 -0500

    all tables created

--- app/src/main/java/com/example/ccimp/SQLiteDatabaseOpenHelper.java
+++ app/src/main/java/com/example/ccimp/SQLiteDatabaseOpenHelper.java
@@ -14,14 +14,45 @@ public class SQLiteDatabaseOpenHelper extends SQLiteOpenHelper {
     public static final String DATABASE_NAME = "CCIMP.db";
 
     // user table
-    public static final String USER_TABLE_NAME = "user";
-    public static final String COL_NAME_EMAIL = "email";
-    public static final String COL_NAME_USERID = "userID";
-    public static final String COL_NAME_PASSWORD = "password";
-    public static final String COL_NAME_TYPE = "accountType";
-    public static final String COL_NAME_ADDRESS = "address";
+    public static final String USER_TABLE_NAME = "User";
+    public static final String COL_NAME_EMAIL = "Email";
+    public static final String COL_NAME_USERID = "UserID";
+    public static final String COL_NAME_PASSWORD = "Password";
+    public static final String COL_NAME_TYPE = "AccountType";
+    public static final String COL_NAME_ADDRESS = "Address";
 
     // business inventory table
+    public static final String BUS_INVENTORY_TABLE_NAME = "BusinessInventory";
+    public static final String COL_NAME_ITEMID = "ItemID";
+    public static final String COL_NAME_QUANTITY = "Quantity";
+    public static final String COL_NAME_AVAILABLE_QUANTITY = "AvailableQuantity";
+
+    // item object
+    public static final String ITEM_TABLE_NAME = "Item";
+    public static final String COL_NAME_ITEMNAME = "Name";
+    public static final String COL_NAME_ITEMPRICE = "Price";
+    public static final String COL_NAME_CUSTOMdETAIL = "Detail";
+
+    // customer order
+    public static final String CUSTOMER_ORDER_TABLE_NAME = "CustomerOrder";
+    public static final String COL_NAME_ORDERID = "OrderID";
+    public static final String COL_NAME_CREATE_TIME = "CreateTime";
+    public static final String COL_NAME_STATUS = "Status";
+    public static final String COL_NAME_TOTAL = "TotalPrice";
+
+    // supplier inventory
+    public static final String SUP_INVENTORY_TABLE_NAME = "SupplierInventory";
+    public static final String COL_NAME_FOOD_NAME= "FoodName";
+    public static final String COL_NAME_SALESPRICE = "SalesPrice";
+
+    // order info
+    public static final String ORDER_INFO_TABLE_NAME = "OrderInfo";
+
+    // business request
+    public static final String BUS_REQUEST_TABLE_NAME = "BusinessRequest";
+    public static final String COL_NAME_REQUEST_DATE = "RequestDate";
+    public static final String COL_NAME_NEEDBY_DATE = "NeedByDate";
+
 
 
 
@@ -38,11 +69,59 @@ public class SQLiteDatabaseOpenHelper extends SQLiteOpenHelper {
                 COL_NAME_PASSWORD + " TEXT," +
                 COL_NAME_TYPE + " TEXT," +
                 COL_NAME_ADDRESS + " TEXT)");
+
+        db.execSQL("create table " + BUS_INVENTORY_TABLE_NAME + " (" +
+                COL_NAME_USERID + " INTEGER PRIMARY KEY," +
+                COL_NAME_ITEMID + " TEXT," +
+                COL_NAME_QUANTITY + " INTEGER," +
+                COL_NAME_AVAILABLE_QUANTITY + " INTEGER)");
+
+        db.execSQL("create table " + ITEM_TABLE_NAME + " (" +
+                COL_NAME_ITEMID + " TEXT PRIMARY KEY," +
+                COL_NAME_ITEMNAME + " TEXT," +
+                COL_NAME_ITEMPRICE + " INTEGER," +
+                COL_NAME_USERID + " INTEGER," +
+                COL_NAME_CUSTOMdETAIL + " TEXT)");
+
+        db.execSQL("create table " + CUSTOMER_ORDER_TABLE_NAME + " (" +
+                COL_NAME_ORDERID + " TEXT PRIMARY KEY," +
+                COL_NAME_CREATE_TIME + " TEXT," +
+                COL_NAME_USERID + " INTEGER," +
+                COL_NAME_USERID + " INTEGER," +
+                COL_NAME_STATUS + " TEXT," +
+                COL_NAME_TOTAL + " INTEGER)");
+
+        db.execSQL("create table " + SUP_INVENTORY_TABLE_NAME + " (" +
+                COL_NAME_FOOD_NAME + " TEXT PRIMARY KEY," +
+                COL_NAME_SALESPRICE + " INTEGER," +
+                COL_NAME_ITEMID + " TEXT," +
+                COL_NAME_USERID + " INTEGER)");
+
+        db.execSQL("create table " + ORDER_INFO_TABLE_NAME + " (" +
+                COL_NAME_ORDERID + " TEXT PRIMARY KEY," +
+                COL_NAME_ITEMID + " TEXT," +
+                COL_NAME_QUANTITY + " INTEGER)");
+
+        db.execSQL("create table " + BUS_REQUEST_TABLE_NAME + " (" +
+                COL_NAME_ORDERID + " TEXT PRIMARY KEY," +
+                COL_NAME_REQUEST_DATE + " TEXT," +
+                COL_NAME_NEEDBY_DATE + " TEXT," +
+                COL_NAME_USERID + " INTEGER," +
+                COL_NAME_USERID + " INTEGER," +
+                COL_NAME_TOTAL + " INTEGER)");
+
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + USER_TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + BUS_INVENTORY_TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + ITEM_TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + CUSTOMER_ORDER_TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + SUP_INVENTORY_TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + ORDER_INFO_TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + BUS_REQUEST_TABLE_NAME);
+
         onCreate(db);
     }
 }
