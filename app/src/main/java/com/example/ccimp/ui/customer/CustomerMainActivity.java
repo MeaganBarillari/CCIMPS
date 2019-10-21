bdf37d8847ce9ca50933bf9b69445e41fd4d2a31
MeaganBarillari <m.barillari7@gmail.com>
2019-10-21 17:51:23 -0500

    creating customer views

--- app/src/main/java/com/example/ccimp/ui/customer/CustomerMainActivity.java
+++ app/src/main/java/com/example/ccimp/ui/customer/CustomerMainActivity.java
@@ -1,14 +1,16 @@
-package com.example.ccimp;
+package com.example.ccimp.ui.customer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.ccimp.R;
+
+public class CustomerMainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_customer_nav);
     }
 }
