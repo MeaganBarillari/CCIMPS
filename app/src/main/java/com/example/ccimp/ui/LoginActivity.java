60bdfe377486d72fa66b94da18a5714992d5fcdc
7nafoaix <shifan.zhou@outlook.com>
2019-10-28 18:15:20 -0500

    updated database

--- app/src/main/java/com/example/ccimp/ui/LoginActivity.java
+++ app/src/main/java/com/example/ccimp/ui/LoginActivity.java
@@ -3,7 +3,28 @@ package com.example.ccimp.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.example.ccimp.R;
 
 public class LoginActivity extends AppCompatActivity {
-    
+    EditText userEmail, password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        userEmail = findViewById(R.id.input_email);
+        password = findViewById(R.id.input_password);
+    }
+
+    public void onLogin(View view) {
+        String email = userEmail.getText().toString();
+        String pass = password.getText().toString();
+        String type = "login";
+
+        backgroundWorker bgworker = new backgroundWorker(this);
+        bgworker.execute(type, email, pass);
+    }
 }
--- app/src/main/java/com/example/ccimp/ui/MainActivity.java
+++ app/src/main/java/com/example/ccimp/ui/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.ccimp.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.ccimp.R;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    Button loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        loginButton = findViewById(R.id.btn_activity_login);
+        loginButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        });
     }
 }
