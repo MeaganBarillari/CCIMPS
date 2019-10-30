297137f06d098e8592b58bcc91d14eb56eb8ddab
7nafoaix <shifan.zhou@outlook.com>
2019-10-30 15:19:03 -0500

    register finished

--- app/src/main/java/com/example/ccimp/ui/MainActivity.java
+++ app/src/main/java/com/example/ccimp/ui/MainActivity.java
@@ -11,7 +11,7 @@ import com.example.ccimp.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button loginButton;
+    Button loginButton, registerButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +22,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        });
+
+        registerButton = findViewById(R.id.btn_activity_register);
+        registerButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
             }
         });
     }
