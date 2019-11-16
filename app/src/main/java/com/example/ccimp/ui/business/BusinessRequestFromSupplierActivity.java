4f0ecf7fd040c57dc12a2a7b69063ed826cc54bb
MeaganBarillari <m.barillari7@gmail.com>
2019-11-15 18:13:57 -0600

    removed old stuff that when i merge with williamtest2 might screwup anything

--- app/src/main/java/com/example/ccimp/ui/business/BusinessRequestFromSupplierActivity.java
+++ app/src/main/java/com/example/ccimp/ui/business/BusinessRequestFromSupplierActivity.java
@@ -11,37 +11,5 @@ import com.example.ccimp.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class BusinessRequestFromSupplierActivity extends AppCompatActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_request_from_supplier);
-
-
-        BottomNavigationView navigation = findViewById(R.id.businessNavigation);
-        navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.navigation_home:
-                        Intent c = new Intent(BusinessRequestFromSupplierActivity.this, BusinessHomeActivity.class);
-                        startActivity(c);
-                        break;
-                    case R.id.navigation_requests:
-                        Intent a = new Intent(BusinessRequestFromSupplierActivity.this, BusinessRequestsActivity.class);
-                        startActivity(a);
-                        break;
-                    case R.id.navigation_inventory:
-                        Intent b = new Intent(BusinessRequestFromSupplierActivity.this, BusinessInventoryActivity.class);
-                        startActivity(b);
-                        break;
-                    case R.id.navigation_business_profile:
-                        Intent d = new Intent(BusinessRequestFromSupplierActivity.this, BusinessProfileActivity.class);
-                        startActivity(d);
-                        break;
-                }
-                return false;
-            }
-        });
-    }
 
 }
