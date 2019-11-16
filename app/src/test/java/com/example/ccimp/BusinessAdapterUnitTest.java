4308a78503545f6869aaddc6353172d27a180f8e
Rzchlab-3 <=>
2019-11-15 22:28:11 -0600

    updated business adapter unit tests format

--- app/src/test/java/com/example/ccimp/BusinessAdapterUnitTest.java
+++ app/src/test/java/com/example/ccimp/BusinessAdapterUnitTest.java
@@ -4,10 +4,14 @@ import android.view.View;
 
 import com.example.ccimp.ui.business.BusinessHomeActivity;
 import com.example.ccimp.ui.business.BusinessRequestCartActivity;
+import com.example.ccimp.ui.business.BusinessRequestsActivity;
 import com.example.ccimp.ui.model.Order;
+import com.example.ccimp.ui.model.Request;
 import com.example.ccimp.ui.model.inventory_supplier;
 import com.example.ccimp.ui.presenter.business.BusinessCartAdapter;
 import com.example.ccimp.ui.presenter.business.BusinessCurrentOrderAdapter;
+import com.example.ccimp.ui.presenter.business.BusinessCurrentRequestsAdapter;
+import com.example.ccimp.ui.presenter.business.BusinessHistoryRequestsAdapter;
 
 import org.junit.Test;
 
@@ -19,9 +23,10 @@ public class BusinessAdapterUnitTest {
 
     ArrayList<inventory_supplier> inventoryArrayList;
     ArrayList<Order> orderArrayList;
+    ArrayList<Request> requestArrayList;
 
     @Test
-    public void cartAdapterTest(){
+    public void cartTest(){
         BusinessRequestCartActivity businessRequestCartActivity = new BusinessRequestCartActivity();
         BusinessCartAdapter businessCartAdapter = new BusinessCartAdapter(businessRequestCartActivity, R.layout.rowfourlines, inventoryArrayList);
         try {
@@ -32,7 +37,7 @@ public class BusinessAdapterUnitTest {
     }
 
     @Test
-    public void  currentOrderAdapterTest(){
+    public void currentOrderTest(){
         BusinessHomeActivity businessHomeActivity = new BusinessHomeActivity();
         BusinessCurrentOrderAdapter businessCurrentOrderAdapter = new BusinessCurrentOrderAdapter(businessHomeActivity, R.layout.row, orderArrayList);
 
@@ -43,4 +48,29 @@ public class BusinessAdapterUnitTest {
         catch(RuntimeException e){}
 
     }
+
+    @Test
+    public void currentRequestsTest(){
+        BusinessRequestsActivity businessRequestsActivity = new BusinessRequestsActivity();
+        BusinessCurrentRequestsAdapter businessCurrentRequestsAdapter = new BusinessCurrentRequestsAdapter(businessRequestsActivity, R.layout.rowtwolines, requestArrayList);
+
+        try{
+            View testView = businessCurrentRequestsAdapter.getView(0, null, null);
+            assertNotNull(testView);
+        }
+        catch(RuntimeException e){}
+    }
+
+    @Test
+    public void historyRequestsTest(){
+        BusinessRequestsActivity businessRequestsActivity = new BusinessRequestsActivity();
+        BusinessHistoryRequestsAdapter businessHistoryRequestsAdapter = new BusinessHistoryRequestsAdapter(businessRequestsActivity, R.layout.row, requestArrayList);
+
+        try{
+            View testView = businessHistoryRequestsAdapter.getView(0, null, null);
+            assertNotNull(testView);
+        }
+        catch(RuntimeException e){}
+    }
+
 }
