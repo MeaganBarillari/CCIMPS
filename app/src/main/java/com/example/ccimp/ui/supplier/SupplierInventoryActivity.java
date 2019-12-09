35fc3ac871cc5606dc4477dce348eae7f234808c
Shifan Zhou <szhou235@wisc.edu>
2019-12-08 22:01:44 -0600

    daily commit

--- app/src/main/java/com/example/ccimp/ui/supplier/SupplierInventoryActivity.java
+++ app/src/main/java/com/example/ccimp/ui/supplier/SupplierInventoryActivity.java
@@ -1,17 +1,23 @@
 package com.example.ccimp.ui.supplier;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.ccimp.R;
 import com.example.ccimp.ui.interfaces.supplier.SupplierInventoryInterface;
+import com.example.ccimp.ui.model.Item;
 import com.example.ccimp.ui.model.User;
 import com.example.ccimp.ui.model.inventory_supplier;
 import com.example.ccimp.ui.presenter.supplier.SupplierInventoryAdapter;
@@ -19,6 +25,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.example.ccimp.ui.presenter.supplier.SupplierInventoryPresenter;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class SupplierInventoryActivity extends AppCompatActivity implements SupplierInventoryInterface.SupplierInventoryView {
 
@@ -49,7 +56,8 @@ public class SupplierInventoryActivity extends AppCompatActivity implements Supp
             btnAddItem.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-
+                    Intent intent = new Intent(SupplierInventoryActivity.this, SupplierAddToInventory.class);
+                    startActivity(intent);
                 }
             });
 
@@ -96,5 +104,36 @@ public class SupplierInventoryActivity extends AppCompatActivity implements Supp
     public void setupInventoryList(ArrayList<inventory_supplier> inventoryArrayList) {
         supplierInventoryAdapter = new SupplierInventoryAdapter(this, R.layout.rowtwolines, inventoryArrayList);
         listView.setAdapter(supplierInventoryAdapter);
+    }
+
+    class supplierInventoryAdapter extends ArrayAdapter<Item> {
+        private List<Item> itemList;
+        private Context ctx;
+        public supplierInventoryAdapter(List<Item> P, Context c){
+            super(c, R.layout.activity_supplier_inventory, P);
+            this.itemList = P;
+            this.ctx = c;
+        }
+
+        @Override
+        public View getView(int position, View mView, ViewGroup parent) {
+            LayoutInflater inflater = LayoutInflater.from(ctx);
+            View view = mView;
+
+            if(view == null) {
+                view = inflater.inflate(R.layout.rowtwolines, parent, false);
+
+            }
+            TextView itemName = view.findViewById(R.id.column1);
+            TextView price = view.findViewById(R.id.column2);
+
+
+            Item item = itemList.get(position);
+            itemName.setText(item.getName());
+            price.setText(item.getPrice());
+
+
+            return view;
+        }
     }
 }
