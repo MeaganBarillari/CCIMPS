053909a14e981477f5dfae95b2206a1e204abe16
Brandon Buchta <brandonbuchta@Brandons-MacBook-Pro.local>
2019-12-13 12:19:09 -0600

    Test automatic testing

--- app/src/androidTest/java/com/example/ccimp/BusinessActivityInstrumentedTest.java
+++ app/src/androidTest/java/com/example/ccimp/BusinessActivityInstrumentedTest.java
@@ -17,7 +17,7 @@ import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
 public class BusinessActivityInstrumentedTest {
 
-    //change 2
+    //change 3
     @Rule
     public ActivityScenarioRule<LoginActivity> login = new ActivityScenarioRule<>(LoginActivity.class);
 
