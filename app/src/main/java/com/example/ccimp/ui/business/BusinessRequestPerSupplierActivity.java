5a04d0558f874ba7fef52a9ef931d78ca8b5d425
rshih2 <37032912+rshih2@users.noreply.github.com>
2019-12-13 10:46:48 -0600

    Update BusinessRequestPerSupplierActivity.java

--- app/src/main/java/com/example/ccimp/ui/business/BusinessRequestPerSupplierActivity.java
+++ app/src/main/java/com/example/ccimp/ui/business/BusinessRequestPerSupplierActivity.java
@@ -5,15 +5,19 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.ccimp.R;
+import com.example.ccimp.ui.customer.CustomerMenuActivity;
+import com.example.ccimp.ui.customer.CustomerOrderCartActivity;
 import com.example.ccimp.ui.interfaces.business.BusinessInventoryInterface;
 import com.example.ccimp.ui.interfaces.business.BusinessRequestPerSupplierInterface;
 import com.example.ccimp.ui.model.User;
@@ -24,6 +28,7 @@ import com.example.ccimp.ui.presenter.business.BusinessRequestPerSupplierAdapter
 import com.example.ccimp.ui.presenter.business.BusinessRequestPerSupplierPresenter;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
 public class BusinessRequestPerSupplierActivity extends AppCompatActivity implements BusinessRequestPerSupplierInterface.BusinessRequestPerSupplierView{
@@ -34,10 +39,21 @@ public class BusinessRequestPerSupplierActivity extends AppCompatActivity implem
     private BusinessRequestPerSupplierAdapter businessRequestPerSupplierAdapter;
     private BusinessRequestPerSupplierInterface.BusinessRequestPerSupplierPresenter businessRequestPerSupplierPresenter;
 
+    ArrayList<inventory_business> cart;
+    int price;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_request_per_supplier);
+
+        price = 0;
+
+//        final Intent intent = getIntent();
+//        businessID = intent.getStringExtra("businessID");
+//        System.out.println(businessID);
+//        customer = intent.getParcelableExtra("customer");
+//        businessName = intent.getStringExtra("businessname");
 
         btnBack = findViewById(R.id.btBack);
         btnCart = findViewById(R.id.btCart);
@@ -57,7 +73,15 @@ public class BusinessRequestPerSupplierActivity extends AppCompatActivity implem
         btnCart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(BusinessRequestPerSupplierActivity.this, BusinessRequestCartActivity.class));
+//                Intent cartI = new Intent(BusinessRequestPerSupplierActivity.this, BusinessRequestCartActivity.class);
+//                cartI.putExtra("businessID", businessID);
+//                cartI.putExtra("customer", customer);
+//                cartI.putExtra("businessname", businessName);
+//                cartI.putExtra("price" , Integer.toString(price));
+//                Bundle args = new Bundle();
+//                args.putSerializable("ARRAYLIST",(Serializable) cart);
+//                cartI.putExtra("BUNDLE",args);
+//                startActivity(cartI);
             }
         });
 
@@ -69,6 +93,17 @@ public class BusinessRequestPerSupplierActivity extends AppCompatActivity implem
         });
 
         listView = findViewById(R.id.suppliermenu);
+
+//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                inventory_business  item = (inventory_business) parent.getItemAtPosition(position);
+//                Toast toast=Toast. makeText(getApplicationContext(),"Item " + item.getItemName() + " added to cart",Toast. LENGTH_SHORT);
+//                toast.show();
+//                cart.add(item);
+//                price = price + Integer.valueOf(item.getPrice());
+//            }
+//        });
 
         businessRequestPerSupplierPresenter.onViewCreate();
 
