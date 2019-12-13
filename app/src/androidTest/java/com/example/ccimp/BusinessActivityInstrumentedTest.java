7801790a3b16a2818b1f9e0c1a84d97b265cb407
Brandon Buchta <brandonbuchta@Brandons-MacBook-Pro.local>
2019-12-13 13:05:50 -0600

    6 to 7

--- app/src/androidTest/java/com/example/ccimp/BusinessActivityInstrumentedTest.java
+++ app/src/androidTest/java/com/example/ccimp/BusinessActivityInstrumentedTest.java
@@ -17,7 +17,7 @@ import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
 public class BusinessActivityInstrumentedTest {
 
-    //change 6
+    //change 7
     @Rule
     public ActivityScenarioRule<LoginActivity> login = new ActivityScenarioRule<>(LoginActivity.class);
 
